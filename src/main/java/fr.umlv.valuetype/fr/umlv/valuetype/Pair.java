ae00c07f8e2d5c884b1c0e2c8e2aa563b6750917
forax <forax@univ-mlv.fr>
2018-07-15 15:28:13 +0200

    test generics with constructor rewriting

---
+++ src/main/java/fr.umlv.valuetype/fr/umlv/valuetype/Pair.java
@@ -0,0 +1,15 @@
+package fr.umlv.valuetype;
+
+public __ByValue class Pair<A, B> {
+  private final A first;
+  private final B second;
+  
+  public Pair(A first, B second) {
+    this.first = first;
+    this.second = second;
+  }
+  
+  public static <A, B> Pair<A, B> of(A first, B second) {
+    return new Pair<>(first, second);
+  }
+}
