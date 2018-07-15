091e2c502b0d365c13f08ad0a1023efe4093e442
forax <forax@univ-mlv.fr>
2018-07-15 15:28:38 +0200

    test inner/outer relationship with value types

---
+++ src/main/java/fr.umlv.valuetype/fr/umlv/valuetype/Outer.java
@@ -0,0 +1,23 @@
+package fr.umlv.valuetype;
+
+public __ByValue class Outer {
+  private final  int value;
+  
+  public Outer(int value) {
+    this.value = value;
+  }
+  
+  public __ByValue class Inner {
+    private final int value2;
+    
+    public Inner(int value2) {
+      this.value2 = value2;
+    }
+  }
+  
+  public static void main(String[] args) {
+    var outer = new Outer(3);
+    var inner = outer.new Inner(4);
+    System.out.println(inner);
+  }
+}
