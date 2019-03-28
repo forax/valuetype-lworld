32c34f632b1c0363e9d954e7854836c14b859c52
forax <forax@univ-mlv.fr>
2019-03-28 15:10:42 +0100

    build.pro -> build.java

---
+++ build.java
@@ -0,0 +1,48 @@
+import static com.github.forax.pro.Pro.*;
+import static com.github.forax.pro.builder.Builders.*;
+import java.util.List;
+
+class build { public static void main(String[] args) {
+    
+pro.loglevel("verbose");
+
+resolver.
+  //checkForUpdate(true).
+  dependencies(
+    // JUnit 5
+    "org.junit.jupiter.api=org.junit.jupiter:junit-jupiter-api:5.2.0",
+    "org.junit.jupiter.params=org.junit.jupiter:junit-jupiter-params:5.2.0",
+    "org.junit.platform.commons=org.junit.platform:junit-platform-commons:1.2.0",
+    "org.apiguardian.api=org.apiguardian:apiguardian-api:1.0.0",
+    "org.opentest4j=org.opentest4j:opentest4j:1.1.0",
+
+    // JMH
+    "org.openjdk.jmh=org.openjdk.jmh:jmh-core:1.21",
+    "org.apache.commons.math3=org.apache.commons:commons-math3:3.3.2",
+    "net.sf.jopt-simple=net.sf.jopt-simple:jopt-simple:4.6",
+    "org.openjdk.jmh.generator=org.openjdk.jmh:jmh-generator-annprocess:1.21"
+    );
+
+compiler.
+  sourceRelease(13).
+  processorModuleTestPath(path("deps")). // enable JMH annotation processor
+  rawArguments(
+    "-Xlint:all",
+    "-XDallowGenericsOverValues",
+    "-XDallowEmptyValues",
+    "-XDallowWithFieldOperator"   
+    );
+
+tester.
+  parallel(false);
+
+packager.
+  modules("fr.umlv.valuetype@1.0/fr.umlv.valuetype.Main");   
+
+run(resolver, modulefixer, compiler, tester, packager /*, perfer */);
+
+List.of(args).forEach(plugin -> run(plugin));   // run command line defined plugins
+
+System.exit(errorCode());
+
+}}
