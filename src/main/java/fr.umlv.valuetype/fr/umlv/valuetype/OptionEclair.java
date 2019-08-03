122156edfcbd7ccae9e6c625998bc970e61c5b8d
forax <forax@univ-mlv.fr>
2019-08-03 19:32:00 +0200

    reformat code

--- src/main/java/fr.umlv.valuetype/fr/umlv/valuetype/OptionEclair.java
+++ src/main/java/fr.umlv.valuetype/fr/umlv/valuetype/OptionEclair.java
@@ -8,98 +8,109 @@ import java.util.function.Supplier;
 
 public interface OptionEclair<E> {
 	public static <E> OptionEclair<E> empty() {
-    return val.empty();
-  }
-  
-  public static <E> OptionEclair<E> of(E value) {
-    return val.of(value);
-  }
-  
-  public static <E> OptionEclair<E> ofNullable(E value) {
-    return val.ofNullable(value);
-  }
-	  
-	  public boolean isPresent();
-	  public boolean isAbsent();
-	  public void ifPresent(Consumer<? super E> consumer);
-	  public E orElse(E defaultValue);
-	  public E orElseGet(Supplier<? extends E> supplier);
-	  public OptionEclair<E> or(Supplier<? extends OptionEclair<E>> supplier);
-	  public <R> OptionEclair<R> flatMap(Function<? super E, ? extends OptionEclair<R>> mapper);
-	  public OptionEclair<E> filter(Predicate<? super E> predicate);
-	  public <R> OptionEclair<R> map(Function<? super E, ? extends R> mapper);
-	  
-	  @__inline__
-	  public final /*inline*/ class val<E> implements OptionEclair<E> {
-	    private final E value;
-	    
-	    private val(E value) {
-	      this.value = value;
-	    }
-	    
-	    public static <E> val<E> empty() {
+		return val.empty();
+	}
+
+	public static <E> OptionEclair<E> of(E value) {
+		return val.of(value);
+	}
+
+	public static <E> OptionEclair<E> ofNullable(E value) {
+		return val.ofNullable(value);
+	}
+
+	public boolean isPresent();
+
+	public boolean isAbsent();
+
+	public void ifPresent(Consumer<? super E> consumer);
+
+	public E orElse(E defaultValue);
+
+	public E orElseGet(Supplier<? extends E> supplier);
+
+	public OptionEclair<E> or(Supplier<? extends OptionEclair<E>> supplier);
+
+	public <R> OptionEclair<R> flatMap(Function<? super E, ? extends OptionEclair<R>> mapper);
+
+	public OptionEclair<E> filter(Predicate<? super E> predicate);
+
+	public <R> OptionEclair<R> map(Function<? super E, ? extends R> mapper);
+
+	@__inline__
+	public final /* inline */ class val<E> implements OptionEclair<E> {
+		private final E value;
+
+		private val(E value) {
+			this.value = value;
+		}
+
+		public static <E> val<E> empty() {
 	      return val<E>.default;
 	    }
-	    
-	    public static <E> val<E> of(E value) {
-	      Objects.requireNonNull(value);
-	      return new val<>(value);
-	    }
-	    
-	    public static <E> val<E> ofNullable(E value) {
-	      return (value == null)? empty(): of(value);
-	    }
-	    
-	    @Override
-			public boolean isPresent() {
-	      return value != null;
-	    }
-	    @Override
-			public boolean isAbsent() {
-	      return value == null;
-	    }
-	    
-	    @Override
-			public void ifPresent(Consumer<? super E> consumer) {
-	      Objects.requireNonNull(consumer);
-	      if (value != null) {
-	        consumer.accept(value);
-	      }
-	    }
-	    
-	    @Override
-			public E orElse(E defaultValue) {
-	      return (value == null)? defaultValue: value;
-	    }
-	    
-	    @Override
-			public E orElseGet(Supplier<? extends E> supplier) {
-	      Objects.requireNonNull(supplier);
-	      return (value == null)? supplier.get(): value;
-	    }
-	    
-			@Override
-			@SuppressWarnings("unchecked")
-			public val<E> or(Supplier<? extends OptionEclair<E>> supplier) {
-	      Objects.requireNonNull(supplier);
-	      return (value == null)? (val<E>)supplier.get(): this;
-	    }
-	    
-	    @Override
-	    @SuppressWarnings("unchecked")
-			public <R> val<R> flatMap(Function<? super E, ? extends OptionEclair<R>> mapper) {
-	      Objects.requireNonNull(mapper);
-	      return (value == null)? empty(): (val<R>)mapper.apply(value);
-	    }
-	    @Override
-			public val<E> filter(Predicate<? super E> predicate) {
-	      Objects.requireNonNull(predicate);
-	      return flatMap(value -> predicate.test(value)? this: empty());
-	    }
-	    @Override
-			public <R> val<R> map(Function<? super E, ? extends R> mapper) {
-	      Objects.requireNonNull(mapper);
-	      return flatMap(value -> OptionEclair.of(mapper.apply(value)));
-	    }
-	  }
+
+		public static <E> val<E> of(E value) {
+			Objects.requireNonNull(value);
+			return new val<>(value);
+		}
+
+		public static <E> val<E> ofNullable(E value) {
+			return (value == null) ? empty() : of(value);
+		}
+
+		@Override
+		public boolean isPresent() {
+			return value != null;
+		}
+
+		@Override
+		public boolean isAbsent() {
+			return value == null;
+		}
+
+		@Override
+		public void ifPresent(Consumer<? super E> consumer) {
+			Objects.requireNonNull(consumer);
+			if (value != null) {
+				consumer.accept(value);
+			}
+		}
+
+		@Override
+		public E orElse(E defaultValue) {
+			return (value == null) ? defaultValue : value;
+		}
+
+		@Override
+		public E orElseGet(Supplier<? extends E> supplier) {
+			Objects.requireNonNull(supplier);
+			return (value == null) ? supplier.get() : value;
+		}
+
+		@Override
+		@SuppressWarnings("unchecked")
+		public val<E> or(Supplier<? extends OptionEclair<E>> supplier) {
+			Objects.requireNonNull(supplier);
+			return (value == null) ? (val<E>) supplier.get() : this;
+		}
+
+		@Override
+		@SuppressWarnings("unchecked")
+		public <R> val<R> flatMap(Function<? super E, ? extends OptionEclair<R>> mapper) {
+			Objects.requireNonNull(mapper);
+			return (value == null) ? empty() : (val<R>) mapper.apply(value);
+		}
+
+		@Override
+		public val<E> filter(Predicate<? super E> predicate) {
+			Objects.requireNonNull(predicate);
+			return flatMap(value -> predicate.test(value) ? this : empty());
+		}
+
+		@Override
+		public <R> val<R> map(Function<? super E, ? extends R> mapper) {
+			Objects.requireNonNull(mapper);
+			return flatMap(value -> OptionEclair.of(mapper.apply(value)));
+		}
+	}
 }
--- src/test/java/fr.umlv.valuetype/fr/umlv/valuetype/perf/OptionBenchMark.java
+++ src/test/java/fr.umlv.valuetype/fr/umlv/valuetype/perf/OptionBenchMark.java
@@ -25,68 +25,62 @@ import fr.umlv.valuetype.OptionEclair;
 @SuppressWarnings("static-method")
 @Warmup(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
-@Fork(value = 3, jvmArgsAppend = {"-XX:+EnableValhalla"})
+@Fork(value = 3, jvmArgsAppend = { "-XX:+EnableValhalla" })
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
 public class OptionBenchMark {
-  private static final Map<String, Integer> MAP = Map.of("foo", 1, "bar", 2, "baz", 3);
-  private static final List<String> LIST = List.of("a", "foo", "b", "bar", "c", "baz");
-  
-  @Benchmark
-  public int sum_null() {
-    int sum = 0;
-    for(var key: LIST) {
-      var value = MAP.get(key);
-      sum += (value != null)? value: 0;
-    }
-    return sum;
-  }
-  
-  @Benchmark
-  public int sum_option() {
-    int sum = 0;
-    for(var key: LIST) {
-      var value = Option.ofNullable(MAP.get(key));
-      sum += value.orElse(0);
-    }
-    return sum;
-  }
-  
-  @Benchmark
-  public int sum_optional() {
-    int sum = 0;
-    for(var key: LIST) {
-      var value = Optional.ofNullable(MAP.get(key));
-      sum += value.orElse(0);
-    }
-    return sum;
-  }
-  
-  @Benchmark
-  public int sum_option_itf() {
-    int sum = 0;
-    for(var key: LIST) {
-      var value = OptionEclair.ofNullable(MAP.get(key));
-      sum += value.orElse(0);
-    }
-    return sum;
-  }
-  
-  /*@Benchmark
-  public int sum_option_itf_val() {
-    int sum = 0;
-    for(var key: LIST) {
-      var value = OptionEclair.val.ofNullable(MAP.get(key));
-      sum += value.orElse(0);
-    }
-    return sum;
-  }*/
-  
-  public static void main(String[] args) throws RunnerException {
-    Options opt = new OptionsBuilder()
-        .include(OptionBenchMark.class.getName())
-        .build();
-    new Runner(opt).run();
-  }
+	private static final Map<String, Integer> MAP = Map.of("foo", 1, "bar", 2, "baz", 3);
+	private static final List<String> LIST = List.of("a", "foo", "b", "bar", "c", "baz");
+
+	@Benchmark
+	public int sum_null() {
+		int sum = 0;
+		for (var key : LIST) {
+			var value = MAP.get(key);
+			sum += (value != null) ? value : 0;
+		}
+		return sum;
+	}
+
+	@Benchmark
+	public int sum_option() {
+		int sum = 0;
+		for (var key : LIST) {
+			var value = Option.ofNullable(MAP.get(key));
+			sum += value.orElse(0);
+		}
+		return sum;
+	}
+
+	@Benchmark
+	public int sum_optional() {
+		int sum = 0;
+		for (var key : LIST) {
+			var value = Optional.ofNullable(MAP.get(key));
+			sum += value.orElse(0);
+		}
+		return sum;
+	}
+
+	@Benchmark
+	public int sum_option_itf() {
+		int sum = 0;
+		for (var key : LIST) {
+			var value = OptionEclair.ofNullable(MAP.get(key));
+			sum += value.orElse(0);
+		}
+		return sum;
+	}
+
+	/*
+	 * @Benchmark public int sum_option_itf_val() { int sum = 0; for(var key: LIST)
+	 * { var value = OptionEclair.val.ofNullable(MAP.get(key)); sum +=
+	 * value.orElse(0); } return sum; }
+	 */
+
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder().include(OptionBenchMark.class.getName()).build();
+		new Runner(opt).run();
+	}
 }
