c8ffbc4bd956491fc83d0d07e1344519fd35abb7
forax <forax@univ-mlv.fr>
2019-08-05 13:26:10 +0200

    add a non-mutable list that avoid using an array if there are less than 4 elements

---
+++ src/main/java/fr.umlv.valuetype/fr/umlv/valuetype/CompactList.java
@@ -0,0 +1,194 @@
+package fr.umlv.valuetype;
+
+import static java.util.Arrays.copyOf;
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.IntStream.range;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.IntFunction;
+
+public @__inline__ class CompactList<E> implements Iterable<E> {
+	private final E[] array;
+	private final E embedded0;
+  private final E embedded1;
+  private final E embedded2;
+  private final E embedded3;
+  private final int size;
+  
+  private CompactList(E[] array, E embedded0, E embedded1, E embedded2, E embedded3, int size) {
+		this.array = array;
+		this.embedded0 = embedded0;
+		this.embedded1 = embedded1;
+		this.embedded2 = embedded2;
+		this.embedded3 = embedded3;
+		this.size = size;
+	}
+
+  public int size() {
+  	return size;
+  }
+  
+	public E get(int index) {
+		if (index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		if (index == 0) {
+			return embedded0;
+  	}
+		if (index == 1) {
+  		return embedded1;
+  	}
+		if (index == 2) {
+  		return embedded2;
+  	}
+		if (index == 3) {
+  		return embedded3;
+  	}
+  	return array[index];
+  }
+	
+	@Override
+	public String toString() {
+		return range(0, size).mapToObj(this::get).map(Object::toString).collect(joining());
+	}
+	
+	@Override
+	public Iterator<E> iterator() {
+		var size = this.size;
+		if (size <= 4) {
+			return new Iterator<>() {
+				private int index;
+				
+				@Override
+				public boolean hasNext() {
+					return index < size;
+				}
+				@Override
+				public E next() {
+					try {
+					  return get(index++);
+					} catch(@SuppressWarnings("unused") IndexOutOfBoundsException e) {
+						throw new NoSuchElementException();
+					}
+				}
+			};
+		}
+		var array = this.array;
+		return new Iterator<>() {
+			private int index;
+			
+			@Override
+			public boolean hasNext() {
+				return index < array.length;
+			}
+			@Override
+			public E next() {
+				try {
+					return array[index++];
+				} catch(@SuppressWarnings("unused") ArrayIndexOutOfBoundsException e) {
+					throw new NoSuchElementException();
+				}
+			}
+		};
+	}
+	
+	@SuppressWarnings("unchecked")
+	public CompactList<E> append(E element) {
+		requireNonNull(element);
+		switch(array.length) {
+		case 0:
+			return new CompactList<>(null, element, null, null, null, 1);
+		case 1:
+			return new CompactList<>(null, embedded0, element, null, null, 2);
+		case 2:
+			return new CompactList<>(null, embedded0, embedded1, element, null, 3);
+		case 3:
+			return new CompactList<>(null, embedded0, embedded1, embedded2, element, 4);
+		case 4:
+			return new CompactList<>((E[])new Object[] { embedded0, embedded1, embedded2, embedded3, element}, null, null, null, null, 5);
+	  default: {
+	  	var length = array.length + 1;
+	  	var newArray = copyOf(array, length);
+	  	newArray[array.length] = element;
+	  	return new CompactList<>(newArray, null, null, null, null, length);
+	  }
+		}
+	}
+  
+	@SuppressWarnings("unchecked")
+	public <T> T[] toArray(IntFunction<? extends T[]> arrayCreator) {
+		var size = this.size;
+		T[] array = arrayCreator.apply(size);  // implicit NPE
+		switch(size) {
+		case 0:
+			return array;
+		case 4:
+			array[3] = (T)embedded3;
+			//$FALL-THROUGH$
+		case 3:
+			array[2] = (T)embedded2;
+			//$FALL-THROUGH$
+		case 2:
+			array[1] = (T)embedded1;
+			//$FALL-THROUGH$
+		case 1:
+			array[0] = (T)embedded0;
+			return array;
+		default:
+			System.arraycopy(this.array, 0, array, 0, size);
+			return array;
+		}
+	}
+	
+  public static <E> CompactList<E> of() {
+  	return new CompactList<>(null, null, null, null, null, 0);
+  }
+  public static <E> CompactList<E> of(E e1) {
+  	requireNonNull(e1);
+  	return new CompactList<>(null, e1, null, null, null, 1);
+  }
+  public static <E> CompactList<E> of(E e1, E e2) {
+  	requireNonNull(e1);
+  	requireNonNull(e2);
+  	return new CompactList<>(null, e1, e2, null, null, 2);
+  }
+  public static <E> CompactList<E> of(E e1, E e2, E e3) {
+  	requireNonNull(e1);
+  	requireNonNull(e2);
+  	requireNonNull(e3);
+  	return new CompactList<>(null, e1, e2, e3, null, 3);
+  }
+  public static <E> CompactList<E> of(E e1, E e2, E e3, E e4) {
+  	requireNonNull(e1);
+  	requireNonNull(e2);
+  	requireNonNull(e3);
+  	requireNonNull(e4);
+  	return new CompactList<>(null, e1, e2, e3, e4, 4);
+  }
+  @SafeVarargs
+  public static <E> CompactList<E> of(E... elements) {
+  	switch(elements.length) {
+  	case 0:
+  		return of();
+  	case 1:
+  		return of(elements[0]);
+  	case 2:
+  		return of(elements[0], elements[1]);
+  	case 3:
+  		return of(elements[0], elements[1], elements[2]);
+  	case 4:
+  		return of(elements[0], elements[1], elements[2], elements[3]);
+  	default:
+  		return ofArray(elements);
+  	}
+  }
+  private static <E> CompactList<E> ofArray(E[] elements) {
+  	var length = elements.length;
+		for(var i = 0; i < length; i++) {
+  		requireNonNull(elements[i]);
+  	}
+  	return new CompactList<>(copyOf(elements, length), null, null, null, null, length);
+  }
+}
---
+++ src/main/java/fr.umlv.valuetype/fr/umlv/valuetype/CompactListTests.java
@@ -0,0 +1,161 @@
+package fr.umlv.valuetype;
+
+import static java.util.Collections.nCopies;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("static-method")
+class CompactListTests {
+	@Test
+	void testOf() {
+		var lists = Stream.of(
+				CompactList.of(), CompactList.of("foo"), CompactList.of("foo", "bar"),
+				CompactList.of("foo", "bar", "baz"), CompactList.of("foo", "bar", "baz", "wizz"),
+				CompactList.of("foo", "bar", "baz", "wizz", "buzz")
+				);
+		lists.forEach(Assertions::assertNotNull);
+	}
+	
+	@Test
+	void testOfNull() {
+		Stream<Supplier<CompactList<String>>> suppliers =
+		  Stream.of(
+				() -> CompactList.of((String)null),
+				() -> CompactList.of("foo", null), () -> CompactList.of(null, "bar"),
+				() -> CompactList.of(null, "bar", "baz"), () -> CompactList.of("foo", null, "baz"), () -> CompactList.of("foo", "bar", null),
+				() -> CompactList.of("foo", "bar", "baz", null), () -> CompactList.of("foo", null, "baz", "wizz"), () -> CompactList.of("foo", "bar", null, "wizz"), () -> CompactList.of("foo", "bar", "baz", null),
+				() -> CompactList.of(null, "bar", "baz", "wizz", "buzz"), () -> CompactList.of("foo", null, "baz", "wizz", "buzz"), () -> CompactList.of("foo", "bar", null, "wizz", "buzz"), () -> CompactList.of("foo", "bar", "baz", null, "buzz"), () -> CompactList.of("foo", "bar", "baz", "wizz", null),
+				() -> CompactList.of((String[])null)
+				);
+		assertAll(suppliers.map(supplier -> () -> assertThrows(NullPointerException.class, () -> supplier.get())));
+	}
+	
+	@Test
+	void testSizeWithStrings() {
+		var lists = List.of(
+				CompactList.of(), CompactList.of("foo"), CompactList.of("foo", "bar"),
+				CompactList.of("foo", "bar", "baz"), CompactList.of("foo", "bar", "baz", "wizz"),
+				CompactList.of("foo", "bar", "baz", "wizz", "buzz")
+				);
+    assertAll(IntStream.range(0, lists.size()).mapToObj(i -> () -> assertEquals(i, lists.get(i).size())));		
+	}
+	@Test
+	void testSizeWithInts() {
+		var lists = List.of(
+				CompactList.of(), CompactList.of(1), CompactList.of(1, 2),
+				CompactList.of(1, 2, 3), CompactList.of(1, 2, 3, 4),
+				CompactList.of(1, 2, 3, 4, 5)
+				);
+    assertAll(IntStream.range(0, lists.size()).mapToObj(i -> () -> assertEquals(i, lists.get(i).size())));		
+	}
+	@Test
+	void testOfArraySize() {
+		var lists = List.of(
+				List.of(), List.of("foo"), List.of("foo", "bar"),
+				List.of("foo", "bar", "baz"), List.of("foo", "bar", "baz", "wizz"),
+				List.of("foo", "bar", "baz", "wizz", "buzz")
+				);
+    assertAll(range(0, lists.size()).mapToObj(i -> () -> {
+			var compactList = CompactList.of(lists.get(i).toArray(String[]::new));
+			assertEquals(i, compactList.size());
+		}));		
+	}
+	
+	@Test
+	void testGet() {
+		var lists = List.of(
+				CompactList.of("foo"), CompactList.of("foo", "bar"),
+				CompactList.of("foo", "bar", "baz"), CompactList.of("foo", "bar", "baz", "wizz"),
+				CompactList.of("foo", "bar", "baz", "wizz", "buzz")
+				);
+    assertAll(range(0, lists.size()).mapToObj(i -> () -> assertEquals("foo", lists.get(0))));
+	}
+	@Test
+	void testGetAllElements() {
+		var lists = Stream.of(
+				List.of(), List.of("foo"), List.of("foo", "bar"),
+				List.of("foo", "bar", "baz"), List.of("foo", "bar", "baz", "wizz"),
+				List.of("foo", "bar", "baz", "wizz", "buzz")
+				);
+    assertAll(lists.flatMap(list -> {
+    	var compactList = CompactList.of(list.toArray(String[]::new));
+    	return range(0, list.size()).mapToObj(i -> () -> assertEquals(list.get(i), compactList.get(i)));
+    }));
+	}
+
+	@Test
+	void testToString() {
+		var lists = Stream.of(
+				List.of(), List.of("foo"), List.of("foo", "bar"),
+				List.of("foo", "bar", "baz"), List.of("foo", "bar", "baz", "wizz"),
+				List.of("foo", "bar", "baz", "wizz", "buzz")
+				);
+    assertAll(lists.map(list -> () -> {
+			var compactList = CompactList.of(list.toArray(String[]::new));
+			assertEquals(list.toString(), compactList.toString());
+		}));
+	}
+	
+	@Test
+	void testIterator() {
+		var lists = Stream.of(
+				List.of(), List.of("foo"), List.of("foo", "bar"),
+				List.of("foo", "bar", "baz"), List.of("foo", "bar", "baz", "wizz"),
+				List.of("foo", "bar", "baz", "wizz", "buzz")
+				);
+		assertAll(lists.map(list -> () ->  {
+			var compactList = CompactList.of(list.toArray(String[]::new));
+			var arrayList = new ArrayList<String>();
+			for(var element: compactList) {
+				arrayList.add(element);
+			}
+			assertEquals(list, arrayList);
+		}));
+	}
+
+	@Test
+	void testAppend() {
+		var compacts = range(0, 10).mapToObj(i -> CompactList.of(nCopies(i, 42).toArray(Integer[]::new))).collect(toList());
+		assertAll(range(0, compacts.size() - 1).boxed().flatMap(i -> {
+			var compact = compacts.get(i);
+			var text = compact.toString();
+			return Stream.of(
+					() -> assertEquals(compacts.get(i + 1), compact.append(42)),
+					() -> assertEquals(text, compact.toString())   // no side effect
+					);
+		}));
+	}
+	
+	@Test
+	void testToArray() {
+		var lists = Stream.of(
+				List.of(), List.of("foo"), List.of("foo", "bar"),
+				List.of("foo", "bar", "baz"), List.of("foo", "bar", "baz", "wizz"),
+				List.of("foo", "bar", "baz", "wizz", "buzz")
+				);
+		assertAll(lists.map(list -> () -> {
+			var compact = CompactList.of(list.toArray(String[]::new));
+			assertEquals(list, List.of(compact.toArray(String[]::new)));
+		}));
+	}
+
+	@Test
+	void testOfArrayChangeAfterCreation() {
+		String[] array = { "foo", "bar", "baz", "wizz", "buzz" };
+		var compactList = CompactList.of(array);
+		array[0] = "hell";
+		assertEquals("foo, bar, baz, wizz, buzz", compactList.toString());
+	}
+}
