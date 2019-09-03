c9dc0c3d7c5fa3e06886d628ada0157e949d4fee
forax <forax@univ-mlv.fr>
2019-09-03 09:24:31 +0200

    add an example of value tearing

---
+++ src/main/java/fr.umlv.valuetype/fr/umlv/valuetype/ValueTearing.java
@@ -0,0 +1,36 @@
+package fr.umlv.valuetype;
+
+public class ValueTearing {
+	@__inline__
+  static class Value {
+		private int x;
+		private int y;
+		
+		public Value(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+  public static void main(String[] args) {
+		var array = new Value[1];
+		var zero = new Value(0, 0);
+		var one = new Value(1, 1);
+		new Thread(() -> {
+			for(;;) {
+			  array[0] = zero;
+			}
+	  }).start();
+		new Thread(() -> {
+			for(;;) {
+			  array[0] = one;
+			}
+	  }).start();
+		for(;;) {
+			var val = array[0];
+			if (val != zero && val != one) {
+				throw new AssertionError("oops");
+			}
+		}
+	}
+}
